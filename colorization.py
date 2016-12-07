4c9a5d942925fb0356e49185232cc80a8353bb35
Steven Shi <steven200796@gmail.com>
2016-12-07 01:42:00 -0500

    working version

--- colorization.py
+++ colorization.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-learning_rate = 1e-4
+starter_learning_rate = 2e-3
+learning_rate_decay = 0.9
+decay_steps = 100
 
 # I'm assuming we will resize all images to the same size
 # tf.image.resize_images
@@ -9,7 +11,7 @@ img_h = 64
 img_w = 64
 
 def weight_variable(shape):
-  return tf.Variable(tf.truncated_normal(shape, stddev=.1))
+  return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
 def bias_variable(shape):
   return tf.Variable(tf.zeros(shape))
@@ -17,13 +19,13 @@ def bias_variable(shape):
 x = tf.placeholder(tf.float32, [None, img_h, img_w, 1])
 output = tf.placeholder(tf.float32, [None, img_h, img_w, 3])
 
-filter_size = 5
-conv_size_1 = 256
+filter_size = 2
+conv_size_1 = 64
 conv_size_2 = 128
-conv_size_3 = 64
-conv_size_4 = 32
-conv_size_5 = 32
-hidden_size = 256
+conv_size_3 = 256
+conv_size_4 = 256
+conv_size_5 = 128
+hidden_size = 128
 
 # first convolutional layer
 W_conv1 = weight_variable([filter_size, filter_size, 1, conv_size_1])
@@ -56,19 +58,23 @@ b_fc1 = bias_variable([hidden_size])
 h_pool_flat = tf.reshape(h_conv5, [-1, conv_size_5])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool_flat, W_fc1) + b_fc1)
 
-# dropout
+#dropou# t
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # second feed forward
 W_fc2 = weight_variable([hidden_size, img_h * img_w * 3 / 256])
 b_fc2 = bias_variable([img_h * img_w * 3 / 256])
-result1 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+result1 = tf.matmul(h_fc1, W_fc2) + b_fc2
 result = tf.reshape(result1, [-1, img_h, img_w, 3])
 
 loss = tf.reduce_sum(tf.square(result - output))
 
+global_step = tf.placeholder(tf.int32)
+learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,decay_steps, learning_rate_decay, staircase=True)
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)#, aggregation_method = tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N)
+
+saver = tf.train.Saver()
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -120,10 +126,10 @@ with tf.Session(config=config) as sess:
                 except:
                         continue
 
-        train = image_nums[:50]
+        train = image_nums[:2]
         test = image_nums[:2]
 
-        for batch in range(25):
+        for batch in range(500):
             input_images = []
             output_images = []
             for i in range(len(train)):
@@ -136,13 +142,12 @@ with tf.Session(config=config) as sess:
                 output_images.append(output_image)
 
 
-            feed_dict = {x: input_images, output: output_images, keep_prob: 1}
+            feed_dict = {x: input_images, output: output_images, keep_prob: 1,global_step: batch}
 
-            sess.run(train_step, feed_dict)
+            train_loss, _ = sess.run([loss,train_step], feed_dict)
+            print batch, train_loss
 
-            train_loss = sess.run(loss, feed_dict)
-            print i, train_loss
-
+        save_path = saver.save(sess, "/tmp/model.ckpt")
         print('***************TESTING***************')
         total_loss = 0
         for i in range(len(test)):
@@ -151,13 +156,12 @@ with tf.Session(config=config) as sess:
             input_image = tf.image.resize_images(input_image, tf.constant([img_h, img_w])).eval()
             output_image = tf.image.decode_jpeg(image, channels=3)
             output_image = tf.image.resize_images(output_image, tf.constant([img_h, img_w])).eval()
-            feed_dict = {x: [input_image], output: [output_image], keep_prob: 1}
-            test_loss = sess.run(loss, feed_dict)
+            feed_dict = {x: [input_image], output: [output_image], keep_prob: 1,global_step: batch}
+            test_loss,test_result = sess.run([loss,result], feed_dict)
             total_loss += test_loss
 
             # save result
             print(test[i], test_loss)
-            test_result = sess.run(result, feed_dict)
             test_result = tf.reshape(test_result, [img_h, img_w, 3])
             with open('result{}.jpeg'.format(test[i]), 'w') as f:
                     f.write(tf.image.encode_jpeg(tf.cast(test_result, tf.uint8)).eval())
